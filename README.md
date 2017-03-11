45aab6dc0598f794ab9720747cbf16f3912c4f38
Andreas Zweili <andreas@2li.ch>
2017-03-11 12:28:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+price_checker.py is a script which looks at the prices of a given
+URL and compares them against a decired price. When the price is
+reached it will send an email to a provided address.
---
+++ price_checker.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+url = input("Please enter the url you want to monitor:")
+desired_price = input("Please enter the price you're looking for:")
+user_email = input("Please enter your email address:")
+
+class Website(object)
+
+class Price(objec)
