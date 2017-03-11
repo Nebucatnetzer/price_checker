003f6e29c57a71cf1d38487b12ca7e97a140fb4b
Andreas Zweili <andreas@2li.ch>
2017-03-11 13:15:06 +0100

    update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 price_checker.py is a script which looks at the prices of a given
 URL and compares them against a decired price. When the price is
 reached it will send an email to a provided address.
+
+required packages:
+
+- libxml2-dev libxslt1-dev
--- price_checker.py
+++ price_checker.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
+# get user email -> get url -> get price
+#download website -> search for price -> compare price
+#if price = website.price -> send mail(user.mail, url)
+#
+#
+#
+#
 
-url = input("Please enter the url you want to monitor:")
-desired_price = input("Please enter the price you're looking for:")
-user_email = input("Please enter your email address:")
+from lxml import html
+import requests
 
-class Website(object)
+class User():
+        email = input("Please enter your email address:")
 
-class Price(objec)
+class Website(object):
+        url = input("Please enter the url you want to monitor:")
+        page = requests.get(url)
+        tree = html.fromstring(page.content)
+
+
+class Price(object):
+        desired_price = input("Please enter the price you're looking for:")
