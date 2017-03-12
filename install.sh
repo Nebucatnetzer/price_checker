0d50958df8d0a7fbc36c5c1d561652954b1a5e4b
Andreas Zweili <andreas@2li.ch>
2017-03-12 21:58:34 +0100

    update the install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install python3 libxml2-dev libxslt1-dev
+sudo apt-get install python3 python3-pip libxml2-dev libxslt1-dev zlib1g-dev
 python3 -m venv ../price_checker
 source bin/activate
 pip install lxml requests
