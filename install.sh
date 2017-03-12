35d7869e42f9cca325f089098192fa36d76288fd
Andreas Zweili <andreas@2li.ch>
2017-03-12 21:44:15 +0100

    remove the environment

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install python3 libxml2-dev libxslt1-dev
+python3 -m venv ../price_checker
+source bin/activate
+pip install lxml requests
--- price_checker.sh
+++ price_checker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source bin/activate
-python3 price_checker.py
+./price_checker.py
 exit 0
