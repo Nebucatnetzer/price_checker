9c19ec21b25356b25d78db6c441974551936d7de
Andreas Zweili <andreas@gch-andzwe-debian-01.ch.gudel.com>
2017-03-14 12:42:58 +0100

    extend depencies in instal.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-sudo apt-get install python3 python3-pip 
+sudo apt-get install python3 python3-pip qt5-default libqt5webkit5-dev\
+     build-essential xvfb libxml2-dev libxslt1-dev
 python3 -m venv ../price_checker
 source bin/activate
-pip install -r requirements.txt 
+pip install -r requirements.txt
